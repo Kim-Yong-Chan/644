f9fa098e33de03667c120a4ae61a5310136fd51a
kkeyhit <rp01020113994@gmail.com>
2020-04-05 18:22:29 +0900

    EurekaServer 구현

--- EurekaServer/src/main/java/com/com/teamViewer/eurekaServer/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/com/teamViewer/eurekaServer/EurekaServerApplication.java
@@ -1,9 +1,11 @@
-package com.timeViewer.eurekaServer;
+package com.com.teamViewer.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
