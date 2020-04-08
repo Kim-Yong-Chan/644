8caabc19911bc85f2951caa32c908b10ba9a654f
kkeyhit <rp01020113994@gmail.com>
2020-04-08 14:43:07 +0900

    사용하지 않는 import제거

--- UserServer/src/main/java/com/teamViewer/userServer/repository/UserRepository.java
+++ UserServer/src/main/java/com/teamViewer/userServer/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.teamViewer.userServer.repository;
 
-import java.nio.file.OpenOption;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,7 @@ import com.teamViewer.userServer.model.UserModel;
 /*JPA 에서는 단순히 Repository 인터페이스를 생성한후
 JpaRepository<Entity, 기본키 타입> 을 extends하면 기본적인 Create, Read, Update, Delete가 자동으로 생성된다.*/
 @Repository
-public interface UserRepositoy extends JpaRepository<UserModel, String> {
+public interface UserRepository extends JpaRepository<UserModel, String> {
 	public Optional<UserModel> findByUserId(String userId);
 	public Optional<UserModel> findByName(String name);
 }
