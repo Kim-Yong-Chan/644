1391fc0711a8fbcb163dae3dfd30a1b964daeef3
kkeyhit <rp01020113994@gmail.com>
2020-04-12 12:10:53 +0900

    BFF 추가

---
+++ BFF/src/test/java/com/teamViewer/zuulServer/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamViewer.zuulServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
