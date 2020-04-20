6f23c4986da5804688b873f590d4647b70683ff8
kkeyhit <rp01020113994@gmail.com>
2020-04-20 16:38:21 +0900

    정적파일 위치 수정, mainPage UI수정

--- WebServer/src/main/java/com/teamviewer/WebServer/WebServerApplication.java
+++ WebServer/src/main/java/com/teamviewer/WebServer/WebServerApplication.java
@@ -3,6 +3,7 @@ package com.teamviewer.WebServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableFeignClients
